b194d909e5c9f1d2731783c3eab3df40f2494592
Billy Vong <billyvg@users.noreply.github.com>
2024-03-12 13:46:45 -0400

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
+import { codecovVitePlugin } from "@codecov/vite-plugin";
+
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+
+    // Put the Codecov vite plugin after all other plugins
+    codecovVitePlugin({
+      enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
+      bundleName: "billy-bundle2-test",
+      uploadToken: process.env.CODECOV_TOKEN,
+    }),
+  ],
 })
